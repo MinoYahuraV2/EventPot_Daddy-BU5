6e7ef706e0a69cda71488e4d33cb5e2b08d072c6
MinoYahuraV2 <superpeet9@gmail.com>
2026-09-17 13:56:14 +0700

    add Swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('EventPot API')
+    .setDescription('EventPot Backend API Documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(3000);
 }
-await bootstrap();
+
+bootstrap();
